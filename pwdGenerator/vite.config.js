b41a5e235e753e627e5c2ac7254f7952b09e5113
DURGA9819 <suzanxtha123@gmail.com>
2025-03-21 13:50:43 +0545

    add

---
+++ pwdGenerator/vite.config.js
@@ -0,0 +1,10 @@
+import {defineConfig} from "vite";
+
+export default defineConfig({
+    base:"/amnilAssignment/pwdGenerator",
+    build: {
+        outDir: "dist",
+        emptyOutDir: true,
+    }
+})
+
